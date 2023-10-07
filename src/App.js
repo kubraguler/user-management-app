9b8b2d8c653ba30b1dfd1310c7677a52d4ced9b7
kubraguler <oktnkbr@gmail.com>
2023-10-08 00:36:29 +0100

    Make api call for user list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import UserListing from "./pages/UserListing/UserListing.js";
-import UserDetail from "./pages/UserDetail/UserDetail.js";
+import UserListPage from "./pages/UserListPage/UserListPage.js";
 
 import "./App.scss";
 
 function App() {
+	const [users, setUsers] = useState([]);
+
+	const options = {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+		}
+	};
+
+	useEffect(() => {
+		const getUsers = async () => {
+			const result = await fetch(
+				"https://laravel-api.albrecht.uk.com/api/users/all",
+				options
+			);
+			const users = await result.json();
+			setUsers(users.data);
+		};
+		getUsers();
+	}, []);
+
 	return (
 		<div className="App">
 			<Routes>
-				<Route path="/" element={<UserListing />} />
-				<Route path="/user-detail" element={<UserDetail />} />
+				<Route path="/" element={<UserListPage users={users} />} />
 			</Routes>
 		</div>
 	);
